f0b7bc5a9c20fb6f50bd9a2d8ba56e2af2a3adc2
felipe-treistman <felipe.treistman@gmail.com>
2023-05-29 14:23:30 -0300

    inclusao de teste

---
+++ tests/testthat/test-smap_ons.R
@@ -0,0 +1,12 @@
+test_that("testa rodada 1 dia SMAP/ONS", {
+  inicializacao <- smap_ons.inic(parametros[Nome == "SOBRADINHO"], EbInic = 800, TuInic = 0.15, Supin = 300, Rsup2Inic = 0)
+  precipitacao <- 0.846906
+  evapotranspiracao <- 5.29 * 0.9
+  Emarg <- 5.29
+  saida <- smap_ons.previsao(parametros[Nome == "SOBRADINHO"], inicializacao, precipitacao, evapotranspiracao, Emarg)
+
+  expect_equal(colnames(saida), c("Qcalc", "Rsolo", "Rsup", "Rsup2", "Rsub",
+                       "Es", "Er", "Rec", "Marg", "Ed", "Ed2", "Ed3",
+                       "Eb", "Tu"))
+  expect_equal(as.numeric(saida[1, 1]), 1100)
+})
