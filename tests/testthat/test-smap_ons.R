38d4b1b92dd4cbd99fcf3e0a305bc4fe3cf66156
felipe-treistman <felipe.treistman@gmail.com>
2023-06-02 10:17:53 -0300

    funcao em rodada variaos dias em c++ / testes

--- tests/testthat/test-smap_ons.R
+++ tests/testthat/test-smap_ons.R
@@ -23,16 +23,76 @@ test_that("testa rodada 2 dias SMAP/ONS", {
   expect_equal(as.numeric(saida[2, 1]), 1065.9265)
 })
 
+test_that("testa rodada 2 dias SMAP/ONS", {
+  
+  modelo <- new_modelo_smap_ons(parametros[Nome == "sobradinho"])
+  inicializacao <- smap_ons.inic(modelo, EbInic = 800, TuInic = 0.15, Supin = 300)
+  precipitacao <- c(0.846906, 0.904504)
+  evapotranspiracao <- c(5.29 * 0.9, 5.29 * 0.9)
+  Emarg <- c(5.29, 5.29)
+  saida <- rodada_varios_dias(modelo, inicializacao, precipitacao, evapotranspiracao, Emarg,  numero_dias = 2)
+
+  expect_equal(colnames(saida), c("Qcalc", "Rsolo", "Rsup", "Rsup2", "Rsub",
+                       "Es", "Er", "Rec", "Marg", "Ed", "Ed2", "Ed3",
+                       "Eb", "Tu"))
+  expect_equal(as.numeric(saida[1, 1]), 1100)
+  expect_equal(as.numeric(saida[2, 1]), 1065.9265)
+})
+
+test_that("testa rodada 2 dias SMAP/ONS rotina cpp", {
+  
+  modelo <- new_modelo_smap_ons(parametros[Nome == "sobradinho"])
+  inicializacao <- smap_ons.inic(modelo, EbInic = 800, TuInic = 0.15, Supin = 300)
+  precipitacao <- c(0.846906, 0.904504)
+  evapotranspiracao <- c(5.29 * 0.9, 5.29 * 0.9)
+  Emarg <- c(5.29, 5.29)
+  saida <- rodada_varios_dias_cpp(unlist(modelo),
+            unlist(inicializacao), attributes(modelo)$area, precipitacao,
+            evapotranspiracao, Emarg,  numero_dias = 2)
+
+  expect_equal(colnames(saida), c("Qcalc", "Rsolo", "Rsup", "Rsup2", "Rsub",
+                       "Es", "Er", "Rec", "Marg", "Ed", "Ed2", "Ed3",
+                       "Eb", "Tu"))
+  expect_equal(as.numeric(saida[1, 1]), 1100)
+  expect_equal(as.numeric(saida[2, 1]), 1065.9265)
+})
+
+#  precipitacao <- rep(5,1000)
+#  evapotranspiracao <- precipitacao
+#  Emarg <- evapotranspiracao
+#  vetor1 <- unlist(modelo)
+#  vetor2 <- unlist(inicializacao)
+#  area <- attributes(modelo)$area
+#  bnch <- bench::mark(rodada_varios_dias_cpp(vetor1,
+#            vetor2, area, precipitacao,
+#            evapotranspiracao, Emarg,  1000),
+#            rodada_varios_dias(modelo, inicializacao, precipitacao,
+#            evapotranspiracao, Emarg,  1000))
+
+
 #test_that("testa rodada 2 dias SMAP/ONS", {
   
-#  modelo <- new_modelo_smap_ons(parametros[Nome == "AVERMELHA"])
+#  modelo <- new_modelo_smap_ons(parametros[Nome == "avermelha"])
 #  EbInic <- 100
 #  TuInic <- 0.15
 #  Supin <- 300
 #  precipitacao = historico_precipitacao[id == 1]
-#  evapotranspiracao = historico_precipitacao[id == 1]
+#  evapotranspiracao = historico_etp[id == 1]
 #  evapotranspiracao[, valor := lubridate::month(data)]
-#  vazao = historico_vazao[posto == "AVERMELHA"]
+#  vazao = historico_vazao[posto == "avermelha"]
 #  data_inicio_objetivo <- "2011-01-01"
-#  data_fim_objetivo <- evapotranspiracao[, max(data) - modelo$kt_max] 
+#  kt_max <- sum(modelo$kt[1:2] > 0)
+#  data_fim_objetivo <- evapotranspiracao[, max(data) - kt_max]
+#})
+
+#test_that("testa rodada 2 dias SMAP/ONS", {
+#      aux <- parametros[Nome == "avermelha"]
+#      aux[, valor := valor * 0.5]
+#      limite_inferior <- aux
+#      limite_inferior <- new_modelo_smap_ons(limite_inferior)
+
+#      aux <- parametros[Nome == "avermelha"]
+#      aux[, valor := valor * 1.5]
+#      limite_superior <- aux
+#      limite_superior <- new_modelo_smap_ons(limite_superior)
 #})
