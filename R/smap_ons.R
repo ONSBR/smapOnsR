62f2634c7764805bdd6c004e1f672e4d2f3d0e07
felipe-treistman <felipe.treistman@gmail.com>
2023-06-02 10:14:13 -0300

    eficientizacao rodada diaria / funcao varios dias

--- R/smap_ons.R
+++ R/smap_ons.R
@@ -16,6 +16,13 @@
 #' @export
 
 rodada_diaria <- function(modelo, inicializacao, precipitacao, evapotranspiracao, Emarg, saidaAnterior = matrix(nrow = 0, ncol = 14), dataD = NULL){
+  
+  #Matriz de Saida ----
+  matrizSaida_nom <- c("Qcalc", "Rsolo", "Rsup", "Rsup2", "Rsub",
+                       "Es", "Er", "Rec", "Marg", "Ed", "Ed2", "Ed3",
+                       "Eb", "Tu")
+  matrizSaida <- matrix(nrow = 1, ncol = length(matrizSaida_nom))
+  colnames(matrizSaida) <- matrizSaida_nom
 
   #Coeficientes Ks
   K_kts <- 0.5 ^ (1 / modelo$k_kt)
@@ -32,76 +39,147 @@ rodada_diaria <- function(modelo, inicializacao, precipitacao, evapotranspiracao
 
   #Calculo das funções de transferencia ----
   #Eqs. Referentes ao Manual de Metodologia SMAP
-  Tu <- RsoloInic / modelo$str #Eq.19 Manual
+  matrizSaida[1,14] <- RsoloInic / modelo$str #Eq.19 Manual
 
   if (precipitacao > modelo$ai){
-    Es <- ((precipitacao - modelo$ai) ^ 2) / (precipitacao - modelo$ai + (modelo$str - RsoloInic)) #Eq.11
+    matrizSaida[1,6] <- ((precipitacao - modelo$ai) ^ 2) / (precipitacao - modelo$ai + (modelo$str - RsoloInic)) #Eq.11
   }else{
-    Es <- 0
+    matrizSaida[1,6] <- 0
   }
 
-  if((precipitacao - Es) > evapotranspiracao){
-    Er <- evapotranspiracao
+  if((precipitacao - matrizSaida[1,6]) > evapotranspiracao){
+    matrizSaida[1,7] <- evapotranspiracao
   }else{
-    Er <- (precipitacao-Es) + (evapotranspiracao - (precipitacao - Es)) *Tu  #Eq.12
+    matrizSaida[1,7] <- (precipitacao-matrizSaida[1,6]) + (evapotranspiracao - (precipitacao - matrizSaida[1,6])) * matrizSaida[1,14]  #Eq.12
   }
 
   Capc_tmp <- (modelo$capc / 100) * modelo$str
 
   if(RsoloInic > Capc_tmp){
-    Rec <- (modelo$crec / 100) * Tu * (RsoloInic - Capc_tmp) #Eq.13
+    matrizSaida[1,8] <- (modelo$crec / 100) * matrizSaida[1,14] * (RsoloInic - Capc_tmp) #Eq.13
   }else{
-    Rec <- 0
+    matrizSaida[1,8] <- 0
   }
 
   #4º Reservatorio
   if (RsupInic > modelo$h){
-    Marg <- (RsupInic - modelo$h) * (1 - K_1ts) #Eq.14
+    matrizSaida[1,9] <- (RsupInic - modelo$h) * (1 - K_1ts) #Eq.14
   }else{
-    Marg <- 0
+    matrizSaida[1,9] <- 0
   }
 
-  Ed <- min(c(RsupInic - Marg, modelo$h1)) * (1 - K_2ts) #Eq.15
+  matrizSaida[1,10] <- min(c(RsupInic - matrizSaida[1,9], modelo$h1)) * (1 - K_2ts) #Eq.15
 
-  Ed2 <- Rsup2Inic * (1 - K_3ts) #Eq.17
+  matrizSaida[1,11] <- Rsup2Inic * (1 - K_3ts) #Eq.17
 
-  Ed3 <- max(c(RsupInic - modelo$h1 - Marg, 0)) * (1 - K_2t2s) #Eq.16
+  matrizSaida[1,12] <- max(c(RsupInic - modelo$h1 - matrizSaida[1,9], 0)) * (1 - K_2t2s) #Eq.16
 
-  Eb <- RsubInic * (1 - K_kts) #Eq.18
+  matrizSaida[1,13] <- RsubInic * (1 - K_kts) #Eq.18
 
   #Calculo das variaveis de estado ----
-  Rsolo <- min(c((RsoloInic + precipitacao - Es - Er - Rec), modelo$str))
-  Rsub <- RsubInic + Rec - Eb
-  Rsup_tmp <- ((RsoloInic + precipitacao - Es - Er - Rec) - modelo$str)
-  Rsup <- RsupInic + Es - Marg - Ed - Ed3 + max(c(0, Rsup_tmp))
-  Rsup2 <- max(Rsup2Inic + Marg - Ed2 - Emarg, 0)
+  matrizSaida[1,2] <- min(c((RsoloInic + precipitacao - matrizSaida[1,6] - matrizSaida[1,7] - matrizSaida[1,8]), modelo$str))
+  matrizSaida[1,5] <- RsubInic + matrizSaida[1,8] - matrizSaida[1,13]
+  Rsup_tmp <- ((RsoloInic + precipitacao - matrizSaida[1,6] - matrizSaida[1,7] - matrizSaida[1,8]) - modelo$str)
+  matrizSaida[1,3] <- RsupInic + matrizSaida[1,6] - matrizSaida[1,9] - matrizSaida[1,10] - matrizSaida[1,12] + max(c(0, Rsup_tmp))
+  matrizSaida[1,4] <- max(Rsup2Inic + matrizSaida[1,9] - matrizSaida[1,11] - Emarg, 0)
 
   #Calculo da vazão ----
-  Qcalc <- (Ed + Ed2 + Ed3 + Eb) * attributes(modelo)$area / 86.4
-
-  #Matriz de Saida ----
-  matrizSaida_nom <- c("Qcalc", "Rsolo", "Rsup", "Rsup2", "Rsub",
-                       "Es", "Er", "Rec", "Marg", "Ed", "Ed2", "Ed3",
-                       "Eb", "Tu")
-  matrizSaida <- matrix(nrow = 1, ncol = length(matrizSaida_nom))
-  colnames(matrizSaida) <- matrizSaida_nom
-  matrizSaida[1,1] <- Qcalc
-  matrizSaida[1,2] <- Rsolo
-  matrizSaida[1,3] <- Rsup
-  matrizSaida[1,4] <- Rsup2
-  matrizSaida[1,5] <- Rsub
-  matrizSaida[1,6] <- Es
-  matrizSaida[1,7] <- Er
-  matrizSaida[1,8] <- Rec
-  matrizSaida[1,9] <- Marg
-  matrizSaida[1,10] <- Ed
-  matrizSaida[1,11] <- Ed2
-  matrizSaida[1,12] <- Ed3
-  matrizSaida[1,13] <- Eb
-  matrizSaida[1,14] <- Tu
+  matrizSaida[1,1] <- (matrizSaida[1,10] + matrizSaida[1,11] + matrizSaida[1,12] + matrizSaida[1,13]) * attributes(modelo)$area / 86.4
 
   matrizSaida <- rbind(saidaAnterior, matrizSaida)
   
+}
+
+#' Rodada de varios dias do SMAP
+#'
+#' @param modelo objeto de classe smap_ons
+#' @param inicializacao  Objeto resultante da funcao smap.inicializacao
+#' @param precipitacao Vetor de precipitacao final (ja corrigido e/ou ponderado)
+#' @param evapotranspiracao Vetor de ETo do final(ja corrigido e/ou ponderado)
+#' @param Emarg Vetor da evaporacao final do reservatorio de planicie (ja corrigido e/ou ponderado)
+#' @param numero_dias numero de dias da rodada
+#'
+#' @return Matriz com a vazao calculada e os valores de estado e funcoes de transferencia
+#' @export
+
+rodada_varios_dias <- function(modelo, inicializacao, precipitacao, evapotranspiracao, Emarg, numero_dias){
+  
+  #Matriz de Saida ----
+  matrizSaida_nom <- c("Qcalc", "Rsolo", "Rsup", "Rsup2", "Rsub",
+                      "Es", "Er", "Rec", "Marg", "Ed", "Ed2", "Ed3",
+                      "Eb", "Tu")
+  matrizSaida <- matrix(nrow = numero_dias, ncol = length(matrizSaida_nom))
+  colnames(matrizSaida) <- matrizSaida_nom
+
+  RsoloInic <- inicializacao$RsoloInic
+  RsupInic <- inicializacao$RsupInic
+  RsubInic <- inicializacao$RsubInic
+  Rsup2Inic <- inicializacao$Rsup2Inic
+  for (idia in 1:numero_dias) {
+
+    #Coeficientes Ks
+    K_kts <- 0.5 ^ (1 / modelo$k_kt)
+    K_1ts <- 0.5 ^ (1 / modelo$k1t)
+    K_2ts <- 0.5 ^ (1 / modelo$k2t)
+    K_2t2s <- 0.5 ^ (1 / modelo$k2t2)
+    K_3ts <- 0.5 ^ (1 / modelo$k3t)
+
+    #Calculo das funções de transferencia ----
+    #Eqs. Referentes ao Manual de Metodologia SMAP
+    matrizSaida[idia, 14] <- RsoloInic / modelo$str #Eq.19 Manual
+
+    if (precipitacao[idia] > modelo$ai){
+      matrizSaida[idia, 6] <- ((precipitacao[idia] - modelo$ai) ^ 2) / (precipitacao[idia] - modelo$ai + (modelo$str - RsoloInic)) #Eq.11
+    }else{
+      matrizSaida[idia, 6] <- 0
+    }
+
+    if((precipitacao[idia] - matrizSaida[idia, 6]) > evapotranspiracao[idia]){
+      matrizSaida[idia, 7] <- evapotranspiracao[idia]
+    }else{
+      matrizSaida[idia, 7] <- (precipitacao[idia]-matrizSaida[idia, 6]) + (evapotranspiracao[idia] - (precipitacao[idia] - matrizSaida[idia, 6])) * matrizSaida[idia, 14]  #Eq.12
+    }
+
+    Capc_tmp <- (modelo$capc / 100) * modelo$str
+
+    if(RsoloInic > Capc_tmp){
+      matrizSaida[idia, 8] <- (modelo$crec / 100) * matrizSaida[idia, 14] * (RsoloInic - Capc_tmp) #Eq.13
+    }else{
+      matrizSaida[idia, 8] <- 0
+    }
+
+    #4º Reservatorio
+    if (RsupInic > modelo$h){
+      matrizSaida[idia, 9] <- (RsupInic - modelo$h) * (1 - K_1ts) #Eq.14
+    }else{
+      matrizSaida[idia, 9] <- 0
+    }
+
+    matrizSaida[idia, 10] <- min(c(RsupInic - matrizSaida[idia, 9], modelo$h1)) * (1 - K_2ts) #Eq.15
+
+    matrizSaida[idia, 11] <- Rsup2Inic * (1 - K_3ts) #Eq.17
+
+    matrizSaida[idia, 12] <- max(c(RsupInic - modelo$h1 - matrizSaida[idia, 9], 0)) * (1 - K_2t2s) #Eq.16
+
+    matrizSaida[idia, 13] <- RsubInic * (1 - K_kts) #Eq.18
+
+    #Calculo das variaveis de estado ----
+    matrizSaida[idia, 2] <- min(c((RsoloInic + precipitacao[idia] - matrizSaida[idia, 6] - matrizSaida[idia, 7] - matrizSaida[idia, 8]), modelo$str))
+    matrizSaida[idia, 5] <- RsubInic + matrizSaida[idia, 8] - matrizSaida[idia, 13]
+    Rsup_tmp <- ((RsoloInic + precipitacao[idia] - matrizSaida[idia, 6] - matrizSaida[idia, 7] - matrizSaida[idia, 8]) - modelo$str)
+    matrizSaida[idia, 3] <- RsupInic + matrizSaida[idia, 6] - matrizSaida[idia, 9] - matrizSaida[idia, 10] - matrizSaida[idia, 12] + max(c(0, Rsup_tmp))
+    matrizSaida[idia, 4] <- max(Rsup2Inic + matrizSaida[idia, 9] - matrizSaida[idia, 11] - Emarg[idia], 0)
+
+    #Calculo da vazão ----
+    matrizSaida[idia, 1] <- (matrizSaida[idia, 10] + matrizSaida[idia, 11] + matrizSaida[idia, 12] + matrizSaida[idia, 13]) * attributes(modelo)$area / 86.4
+
+    RsoloInic <- matrizSaida[idia, 2]
+    RsupInic <- matrizSaida[idia, 3]
+    Rsup2Inic <- matrizSaida[idia, 4]
+    RsubInic <- matrizSaida[idia, 5]
+  }
+
+  matrizSaida
 }
 
 #' Construtor do modelo classe \code{smap_ons}
@@ -145,7 +223,7 @@ new_modelo_smap_ons <- function(parametros){
   ecof2 <- parametros[parametro == "Ecof2", valor]
 
   modelo <- list(str = str, k2t = k2t, crec = crec, capc = capc, k_kt = k_kt,
-  h1 = h1, k2t2 = k2t2, ai = ai, h = h, l1t = k1t, k3t = k3t, kt = kt, pcof = pcof,
+  h1 = h1, k2t2 = k2t2, ai = ai, h = h, k1t = k1t, k3t = k3t, kt = kt, pcof = pcof,
   ecof = ecof, ecof2 = ecof2)
 
   attr(modelo, "nome") <- parametros[, unique(Nome)]
