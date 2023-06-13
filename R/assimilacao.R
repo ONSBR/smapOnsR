3f85b94792d856afea483d02d839f283bb36c363
felipe-treistman <felipe.treistman@gmail.com>
2023-06-13 09:19:01 -0300

    consideracao de pesos FO da assimilacao

--- R/assimilacao.R
+++ R/assimilacao.R
@@ -3,7 +3,7 @@
 #' Assimilacao oficial
 #' Realiza a assimilacao de dados com a metodologia oficial
 #'
-#' @param vetorModelo objeto de classe smap_ons
+#' @param vetor_modelo objeto de classe smap_ons
 #' @param area area da sub-bacia
 #' @param Supin Escoamento superficial inicial
 #' @param Rsup2Inic Escoamento superficial inicial referente ao reservatorio de planicie
@@ -46,30 +46,33 @@
 #' @export
 #' 
 
-assimilacao_oficial <- function(vetorModelo, area, EbInic, TuInic, Supin, precipitacao,
+assimilacao_oficial <- function(vetor_modelo, area, EbInic, TuInic, Supin, precipitacao,
       evapotranspiracao, evapotranspiracao_planicie, vazao, numero_dias, 
       limite_prec = c(0.5, 2), limite_ebin = c(0.8, 1.2),
       limite_supin = c(0, 2), data_rodada){
     
-    kt <- vetorModelo[12:74]
+    kt <- vetor_modelo[12:74]
     kt_max <- sum(kt[1:2] > 0)
     kt_min <- sum(kt[4:63] > 0)
     precipitacao_ponderada <- data.table::data.table(precipitacao)
-    precipitacao_ponderada[, valor := valor * vetorModelo[75]]
+    precipitacao_ponderada[, valor := valor * vetor_modelo[75]]
     precipitacao_ponderada <- ponderacao_temporal2(precipitacao_ponderada[, valor], kt,
                                                     kt_max, kt_min)
 
     pesos <- rep(1, numero_dias)
+    limite_inferior <- c(rep(limite_prec[1],numero_dias), limite_ebin[1], limite_supin[1])
+    limite_superior <- c(rep(limite_prec[2],numero_dias), limite_ebin[2], limite_supin[2])
+    limite_inferior[numero_dias] <- 0.9999999999
+    limite_superior[numero_dias] <- 1.0000000001
+    vetor_parametros <- c(pesos, EbInic, Supin)
 
-    limite_inferior = c(rep(limite_prec[1],numero_dias), limite_ebin[1], limite_supin[1])
-    limite_superior = c(rep(limite_prec[2],numero_dias), limite_ebin[2], limite_supin[2])
-
-    vetorParametros <- c(pesos, EbInic, Supin)
-
-    ajuste <- stats::optim(par = vetorParametros, method = "L-BFGS-B",
+    idia <- numero_dias:1
+    pesos_funcao_objetivo <- (log(idia + 1) - log(idia)) / log(numero_dias + 1)
+    
+    ajuste <- stats::optim(par = vetor_parametros, method = "L-BFGS-B",
               lower = limite_inferior, upper = limite_superior,
               fn = funcao_objetivo_assimilacao,
-              vetorModelo = vetorModelo,
+              vetor_modelo = vetor_modelo,
               TuInic = TuInic,
               area = area,
               precipitacao_ponderada = precipitacao_ponderada,
@@ -77,23 +80,22 @@ assimilacao_oficial <- function(vetorModelo, area, EbInic, TuInic, Supin, precip
               evapotranspiracao_planicie = evapotranspiracao_planicie,
               vazao = vazao,
               numero_dias = numero_dias,
+              pesos_funcao_objetivo = pesos_funcao_objetivo,
               control = list(fnscale = 1))
     ajuste
 }
 
 #' funcao objetivo de assimilacao do SMAP/ONS
 
-#' @param vetorModelo vetor resultante de unlist do objeto de classe smap_ons
-#' @param vetorParametros vetor com os parametros:
+#' @param vetor_modelo vetor resultante de unlist do objeto de classe smap_ons
+#' @param vetor_parametros vetor com os parametros:
 #' \itemize{
 #'     \item{pesos}{pesos da ponderacao da precipitacao}
 #'     \item{Ebin}{vazao de base inicial}
 #'     \item{Supin}{vazao superficial inicial}
 #' }
-#' @param pesos vetor de pesos a serem utilizados para a ponderacao da prec na assimilacao
-#' @param EbInic vazao de base inicial
+#' @param pesos_funcao_objetivo vetor de pesos da funcao objetivo
 #' @param TuInic umidade do solo inicial
-#' @param Supin vazao superficial inicial
 #' @param precipitacao Vetor de precipitacao final (ja corrigido e/ou ponderado)
 #' @param evapotranspiracao Vetor de ETo do final(ja corrigido e/ou ponderado)
 #' @param vazao data table com a vazao a avaliada
@@ -110,21 +112,21 @@ assimilacao_oficial <- function(vetorModelo, area, EbInic, TuInic, Supin, precip
 #' @return objetivo valor da funcao objetivo
 #' @export
 
-funcao_objetivo_assimilacao <- function(vetorParametros, vetorModelo, TuInic,
+funcao_objetivo_assimilacao <- function(vetor_parametros, vetor_modelo, TuInic,
       precipitacao_ponderada, evapotranspiracao, evapotranspiracao_planicie, vazao, area,
-      numero_dias){
+      numero_dias, pesos_funcao_objetivo = rep(1 /length(numero_dias), length(numero_dias))){
 
-  EbInic <- vetorParametros[(length(vetorParametros) - 1)]
-  Supin <- vetorParametros[length(vetorParametros)]
-  inicializacao <- smap_ons.inic(vetorModelo, area, EbInic, TuInic, Supin)
+  EbInic <- vetor_parametros[(length(vetor_parametros) - 1)]
+  Supin <- vetor_parametros[length(vetor_parametros)]
+  inicializacao <- smap_ons.inic(vetor_modelo, area, EbInic, TuInic, Supin)
   vetorInicializacao <- unlist(inicializacao)
 
-  precipitacao_ponderada <- precipitacao_ponderada * vetorParametros[1:(length(vetorParametros) - 2)]
+  precipitacao_ponderada <- precipitacao_ponderada * vetor_parametros[1:(length(vetor_parametros) - 2)]
 
-  simulacao <- rodada_varios_dias_cpp(vetorModelo,
+  simulacao <- rodada_varios_dias_cpp(vetor_modelo,
             vetorInicializacao, area, precipitacao_ponderada,
             evapotranspiracao, evapotranspiracao_planicie, numero_dias)
 
-  objetivo <- calcula_dm(simulacao[, 1], vazao)
+  objetivo <- calcula_dm(simulacao[, 1], vazao, pesos_funcao_objetivo)
   objetivo
 }
