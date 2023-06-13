a8a8ca07d48f88150f30ea1413717f5188eb5c94
felipe-treistman <felipe.treistman@gmail.com>
2023-06-13 09:54:15 -0300

    permite limites iguais na otimizacao

--- R/calibracao.R
+++ R/calibracao.R
@@ -1,6 +1,6 @@
 #' funcao objetivo de calibracao do SMAP/ONS
 #'
-#' @param modelo vetor resultante de unlist do objeto de classe smap_ons
+#' @param vetor_modelo vetor resultante de unlist do objeto de classe smap_ons
 #' @param area area da sub-bacia
 #' @param EbInic vazao de base inicial
 #' @param TuInic umidade do solo inicial
@@ -34,31 +34,31 @@
 #' @return objetivo valor da funcao objetivo
 #' @export
 
-funcao_objetivo <- function(modelo, kt_min, kt_max, area, EbInic, TuInic, Supin, 
+funcao_objetivo <- function(vetor_modelo, kt_min, kt_max, area, EbInic, TuInic, Supin, 
       precipitacao, evapotranspiracao, vazao, data_inicio_objetivo, data_fim_objetivo){
 
   numero_kts <- kt_max + kt_min + 1
   quantis <- seq((1 / (numero_kts + 2)), 1, (1 / numero_kts))
-  aux <- stats::dbeta(quantis, shape1 = modelo[15], shape2 = modelo[16])
+  aux <- stats::dbeta(quantis, shape1 = vetor_modelo[15], shape2 = vetor_modelo[16])
   kt <- rep(0, 63)
   kt[(3 - kt_max):(3 + kt_min)] <- aux / sum(aux)
 
   numero_dias <- nrow(evapotranspiracao)
 
-  inicializacao <- smap_ons.inic(modelo, area, EbInic, TuInic, Supin)
+  inicializacao <- smap_ons.inic(vetor_modelo, area, EbInic, TuInic, Supin)
 
   precipitacao_ponderada <- data.table::data.table(precipitacao)
-  precipitacao_ponderada[, valor := valor * modelo[12]]
+  precipitacao_ponderada[, valor := valor * vetor_modelo[12]]
   precipitacao_ponderada <- ponderacao_temporal(precipitacao_ponderada[, valor], kt, kt_max, kt_min)
 
   evapotranspiracao_ponderada <- data.table::data.table(evapotranspiracao)
-  evapotranspiracao_ponderada[, valor := valor * modelo[13]]
+  evapotranspiracao_ponderada[, valor := valor * vetor_modelo[13]]
   evapotranspiracao_planicie_ponderada <- data.table::data.table(evapotranspiracao)
-  evapotranspiracao_planicie_ponderada[, valor := valor * modelo[14]]
+  evapotranspiracao_planicie_ponderada[, valor := valor * vetor_modelo[14]]
 
   vetorInicializacao <- unlist(inicializacao)
 
-  saida <- rodada_varios_dias_cpp(modelo,
+  saida <- rodada_varios_dias_cpp(vetor_modelo,
             vetorInicializacao, area, precipitacao_ponderada,
             evapotranspiracao_ponderada[, valor], evapotranspiracao_planicie_ponderada[, valor], numero_dias)
   
@@ -72,7 +72,7 @@ funcao_objetivo <- function(modelo, kt_min, kt_max, area, EbInic, TuInic, Supin,
 
 #' funcao de calibracao do SMAP/ONS
 #'
-#' @param modelo vetor resultante de unlist do objeto de classe smap_ons
+#' @param vetor_modelo vetor resultante de unlist do objeto de classe smap_ons
 #' @param area area da sub-bacia
 #' @param EbInic vazao de base inicial
 #' @param TuInic umidade do solo inicial
@@ -107,11 +107,15 @@ funcao_objetivo <- function(modelo, kt_min, kt_max, area, EbInic, TuInic, Supin,
 #' @return objetivo valor da funcao objetivo
 #' @export
 #' 
-calibracao <- function(modelo, kt_min, kt_max, area, EbInic, TuInic, Supin, precipitacao,
+calibracao <- function(vetor_modelo, kt_min, kt_max, area, EbInic, TuInic, Supin, precipitacao,
       evapotranspiracao, vazao, data_inicio_objetivo, data_fim_objetivo,
       limite_inferior, limite_superior){
   
-  ajuste <- stats::optim(par = modelo, method = "L-BFGS-B",
+  if(limite_inferior == limite_superior){
+    limite_superior[limite_inferior == limite_superior] <- limite_superior[limite_inferior == limite_superior] + 0.000001
+  }
+  
+  ajuste <- stats::optim(par = vetor_modelo, method = "L-BFGS-B",
               lower = limite_inferior, upper = limite_superior,
               fn = funcao_objetivo,
               kt_min = kt_min,
