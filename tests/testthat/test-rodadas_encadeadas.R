69b6992aeea5d1249fe068642b3ea3235064d923
felipe-treistman <felipe.treistman@gmail.com>
2023-06-16 15:28:30 -0300

    ajuste teste rodada encadeada

--- tests/testthat/test-rodadas_encadeadas.R
+++ tests/testthat/test-rodadas_encadeadas.R
@@ -1,7 +1,10 @@
 test_that("testa rodadas encadeadas", {
     numero_dias_assimilacao <- 31
     
+    datas_rodadas<- data.table::data.table(data = as.Date(c("2020-05-01", "2020-05-08")), numero_dias_previsao = c(15, 20))
+
     sub_bacias <- c("avermelha", "ssimao2")
+    
     precipitacao <- historico_precipitacao[posto %in% postos_plu[nome %in% sub_bacias, posto]]
     previsao_precipitacao <- transforma_historico_previsao(precipitacao, datas_rodadas)
     aux <- data.table::data.table(previsao_precipitacao)
@@ -13,7 +16,6 @@ test_that("testa rodadas encadeadas", {
     evapotranspiracao <- historico_etp_NC[posto %in% sub_bacias]
 
     inicializacao <- data.table::data.table(nome = c("avermelha", "ssimao2"), Ebin = c(218.71, 441.67), Supin = c(46.69, 256.98), Tuin = c(0.2891, 0.3141))
-    datas_rodadas<- data.table::data.table(data = as.Date(c("2020-05-01", "2020-05-08")), numero_dias_previsao = c(15, 20))
     
     saida <- rodada_encadeada_oficial(parametros[Nome %in% sub_bacias],
     inicializacao, precipitacao, previsao_precipitacao, evapotranspiracao, vazao,
