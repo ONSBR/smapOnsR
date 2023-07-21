55eb4f0d79cfe68c32c5743c182f663327a438ef
felipe-treistman <felipe.treistman@gmail.com>
2023-07-21 09:58:33 -0300

    verificacao da existencia dos arquivos de entrada

--- R/leitura.R
+++ R/leitura.R
@@ -242,11 +242,23 @@ le_evapotranspiracao_nc <- function(arq) {
 le_arq_entrada_novo <- function(pasta_entrada){
     arquivos <- le_arquivos(pasta_entrada)
 
-    sub_bacias <- data.table::fread(file.path(pasta_entrada,arquivos[arquivo == "SUB_BACIAS", nome_arquivo]))
+    if (any(arquivos[, arquivo] == "SUB_BACIAS")) {
+        sub_bacias <- data.table::fread(file.path(pasta_entrada,arquivos[arquivo == "SUB_BACIAS", nome_arquivo]))
+    } else {
+        stop("nao existe o arquivo de sub-bacias")
+    }
 
-    parametros <- le_parametros(file.path(pasta_entrada,arquivos[arquivo == "PARAMETROS", nome_arquivo]))[Nome %in% sub_bacias$nome]
+    if (any(arquivos[, arquivo] == "PARAMETROS")) {
+        parametros <- le_parametros(file.path(pasta_entrada,arquivos[arquivo == "PARAMETROS", nome_arquivo]))[Nome %in% sub_bacias$nome]
+    } else {
+        stop("nao existe o arquivo de parametros")
+    }
 
-    vazao_observada <- le_historico_verificado(file.path(pasta_entrada,arquivos[arquivo == "VAZAO_OBSERVADA", nome_arquivo]))[posto %in% sub_bacias$nome]
+    if (any(arquivos[, arquivo] == "VAZAO_OBSERVADA")) {
+        vazao_observada <- le_historico_verificado(file.path(pasta_entrada,arquivos[arquivo == "VAZAO_OBSERVADA", nome_arquivo]))[posto %in% sub_bacias$nome]
+    } else {
+        stop("nao existe o arquivo de vazoes observadas")
+    }
 
     bool_nc_evapotranspiracao <- any(arquivos[, arquivo] == "EVAPOTRANSPIRACAO_NC")
     evapotranspiracao_observada <- data.table::data.table()
@@ -264,14 +276,30 @@ le_arq_entrada_novo <- function(pasta_entrada){
         evapotranspiracao_observada <- le_historico_verificado(file.path(pasta_entrada,arquivos[arquivo == "EVAPOTRANSPIRACAO_OBSERVADA", nome_arquivo]))[posto %in% sub_bacias$nome]
     }
 
-    postos_plu <- le_postos_plu(file.path(pasta_entrada, arquivos[arquivo == "POSTOS_PLUVIOMETRICOS", nome_arquivo]))
+    if (any(arquivos[, arquivo] == "POSTOS_PLUVIOMETRICOS")) {
+        postos_plu <- le_postos_plu(file.path(pasta_entrada, arquivos[arquivo == "POSTOS_PLUVIOMETRICOS", nome_arquivo]))
+    } else{
+        stop("nao existe o arquivo de postos pluviometricos")
+    }
 
-    precipitacao_observada <- le_historico_verificado(file.path(pasta_entrada,arquivos[arquivo == "PRECIPITACAO_OBSERVADA", nome_arquivo]))[posto %in% postos_plu$posto]
+    if (any(arquivos[, arquivo] == "PRECIPITACAO_OBSERVADA")) {
+        precipitacao_observada <- le_historico_verificado(file.path(pasta_entrada,arquivos[arquivo == "PRECIPITACAO_OBSERVADA", nome_arquivo]))[posto %in% postos_plu$posto]
+    } else {
+        stop("nao existe o arquivo de precipitacao observada")
+    }
 
-    inicializacao <-  le_inicializacao(file.path(pasta_entrada,arquivos[arquivo == "INICIALIZACAO", nome_arquivo]))[nome %in% sub_bacias$nome]
-    inicializacao[variavel == "Tuin", valor := valor / 100] 
+    if (any(arquivos[, arquivo] == "INICIALIZACAO")) {
+        inicializacao <-  le_inicializacao(file.path(pasta_entrada,arquivos[arquivo == "INICIALIZACAO", nome_arquivo]))[nome %in% sub_bacias$nome]
+        inicializacao[variavel == "Tuin", valor := valor / 100]
+    } else {
+        stop("nao existe o arquivo de estados iniciais do smap")
+    }
 
-    datas_rodadas <- le_datas_rodada(file.path(pasta_entrada,arquivos[arquivo == "DATAS_RODADAS", nome_arquivo]))
+    if (any(arquivos[, arquivo] == "DATAS_RODADAS")) {
+        datas_rodadas <- le_datas_rodada(file.path(pasta_entrada,arquivos[arquivo == "DATAS_RODADAS", nome_arquivo]))
+    } else{
+        stop("nao existe o arquivo de datas a serem executadas")
+    }
 
     precipitacao_observada <- data.table::rbindlist(lapply(sub_bacias$nome, function(sub_bacia) {
   ponderacao_espacial(precipitacao_observada, postos_plu[nome %in% sub_bacia])
