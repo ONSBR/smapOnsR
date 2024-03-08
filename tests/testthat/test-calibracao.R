66eff127bb6c2c88bfcb6c96ec25182a23296de3
felipe-treistman <felipe.treistman@gmail.com>
2024-03-08 15:19:36 -0300

    teste de calibracao nao paralelo / ajuste teste rodada encadeada

--- tests/testthat/test-calibracao.R
+++ tests/testthat/test-calibracao.R
@@ -51,7 +51,7 @@ test_that("Testa a funcao de calibracao", {
         pesos = rep(1 / length(vazao[, valor]), length(vazao[, valor])), numero_dias, numero_postos_plu)
 
     set.seed(1637)
-    par <- calibracao_paralela(vetor_modelo,  kt_min, kt_max, area, EbInic, TuInic, Supin, precipitacao,
+    par <- calibracao(vetor_modelo,  kt_min, kt_max, area, EbInic, TuInic, Supin, precipitacao,
         evapotranspiracao, vazao, data_inicio_objetivo, data_fim_objetivo,
         limite_inferior, limite_superior, postos_plu[nome == nome2], calcula_dm)
 
